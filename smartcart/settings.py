4e12847651f52358cf79d1c05341d8239e92b6fd
MathanKumar01 <mathanvkp720@gmail.com>
2026-03-13 15:16:57 +0530

    fix database config for render

--- smartcart/settings.py
+++ smartcart/settings.py
@@ -94,14 +94,9 @@ WSGI_APPLICATION = "smartcart.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'smartcart_db',
-        'USER': 'postgres',
-        'PASSWORD': 'selvi@2mathan',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
 
 

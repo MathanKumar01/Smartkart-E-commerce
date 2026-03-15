26bf37418dedf4cc7348f92b9c0bbf2a81f9fb85
MathanKumar01 <mathanvkp720@gmail.com>
2026-03-15 11:11:40 +0530

    Database Changed

--- backend/import_mobiles.py
+++ backend/import_mobiles.py
@@ -6,9 +6,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smartcart.settings")
 django.setup()
 
 from products.models import Product
-if Product.objects.exists():
-    print("Products already exist. Skipping import.")
-    exit()
 
 df = pd.read_csv(r"C:\Users\MathanVini\Downloads\flipkart_mobile_data.csv")
 
@@ -16,26 +13,15 @@ df = df.head(50)
 
 for _, row in df.iterrows():
 
-    # clean price
-    price = row.get("Price", "0")
-    price = str(price).replace("₹", "").replace(",", "")
-    price = float(price)
-
-    description = f"""
-    Brand: {row.get('Brand','')}
-    RAM: {row.get('RAM','')}
-    Storage: {row.get('Storage','')}
-    Battery: {row.get('Battery','')}
-    Camera: {row.get('Camera','')}
-    """
+    price = str(row["Price"]).replace("₹","").replace(",","")
 
     Product.objects.create(
-        name=row.get("Model", "Mobile Phone"),
-        description=description,
-        price=price,
+        name=row["Title"],
+        description=row["Key Features"],
+        price=float(price),
         stock=15,
         image_url=row["Image URL"],
         category="phone"
     )
 
-print("✅ 50 mobile phones imported successfully!")
+print("✅ 50 phones imported successfully!")
--- backend/import_products.py
+++ backend/import_products.py
@@ -11,10 +11,8 @@ if Product.objects.exists():
     print("Products already exist. Skipping import.")
     exit()
 
-
 df = pd.read_csv(r"C:\Users\MathanVini\Downloads\flipkart_laptops (1).csv")
 
-# remove extra spaces in column names
 df.columns = df.columns.str.strip()
 
 df = df.head(50)
@@ -24,11 +22,11 @@ for _, row in df.iterrows():
     price = str(row["Price"]).replace("₹","").replace(",","")
 
     Product.objects.create(
-        name=row["Product Link"][:200],
-        description="Laptop imported from Flipkart dataset",
+        name=row["Title"][:200],
+        description=row["Key Features"],   # better description
         price=float(price),
         stock=10,
-        image_url=row["Image URL"],   # ← dataset image
+        image_url=row["Image URL"],
         category="laptop"
     )
 
